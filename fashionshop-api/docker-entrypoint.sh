fab82bcee90313375d3b9508ce45d77f368467b3
Humanoid1266 <tridq6280@ut.edu.vn>
2026-07-21 08:50:20 +0700

    Prepare Render deploy: env API URL, dynamic port, Aiven CA

--- fashionshop-api/docker-entrypoint.sh
+++ fashionshop-api/docker-entrypoint.sh
@@ -16,4 +16,4 @@ php artisan route:cache
 # Chạy migration
 php artisan migrate --no-interaction --force
 
-exec php artisan serve --host=0.0.0.0 --port=8000
+exec php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
